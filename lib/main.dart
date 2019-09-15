4d9d53316dbfdea9acde89fecc17b328d54c69a4
Nilo Neregato <niloneregato@hotmail.com>
2019-09-15 22:33:30 +0200

    added answer and dynamic buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:complete_guide/answer.dart';
 import 'package:complete_guide/question.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIdex = 0;
 
-  void aswerdQuestion() {
+  void _answerQuestion() {
     /*
     I order to render the screen, besides extends State<>, I need to wrap the logic
     to update the UI by using the method setState(), like
@@ -27,8 +28,18 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?'
+      {
+        'questionText': 'Vilken är din favoritfärg?',
+        'answers': ['Svart', 'Röd', 'Blä', 'Grön']
+      },
+      {
+        'questionText': 'Vilket är ditt favoritdjur?',
+        'answers': ['Anka', 'Älg', 'Häst', 'Hund']
+      },
+      {
+        'questionText': 'Vad är din favoritmat?',
+        'answers': ['Nötkött', 'Pizza', 'Frukt', 'Glass']
+      },
     ];
     return MaterialApp(
       home: Scaffold(
@@ -37,19 +48,12 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(questions[_questionIdex]),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: () => aswerdQuestion(),
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: () => aswerdQuestion(),
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: () => aswerdQuestion(),
-            ),
+            Question(questions[_questionIdex]['questionText']),
+            //... add the values from a list to a list. This avoid to neast a list into another one
+            ...(questions[_questionIdex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
