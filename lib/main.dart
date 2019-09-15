f0c3ccff18b1ba64004a5e0f2b7a8205af582eef
Nilo Neregato <niloneregato@hotmail.com>
2019-09-15 20:36:24 +0200

    implement StatefullWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,56 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIdex = 0;
+
+  void aswerdQuestion() {
+    /*
+    I order to render the screen, besides extends State<>, I need to wrap the logic
+    to update the UI by using the method setState(), like
+    */
+    setState(() {
+      questionIdex += 1;
+    });
+    print(questionIdex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
-      home: Text('Hello!!!'),
-      );
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Playgroung'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIdex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: () => aswerdQuestion(),
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => aswerdQuestion(),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => aswerdQuestion(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
